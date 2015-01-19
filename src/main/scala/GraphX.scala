a628e04b7c7ff0b9a09a42ddf680ed334c27acd0
Nagato KASAKI <kasaki-nagato@dmm.com>
2015-01-19 21:42:40 +0900

    test aggregation

--- src/main/scala/GraphX.scala
+++ src/main/scala/GraphX.scala
@@ -14,20 +14,20 @@ object GraphX {
     val sc = new SparkContext(conf)
     
     // Create an RDD for the vertices
-    val users: RDD[(VertexId, (String, String))] =
-      sc.parallelize(Array((3L, ("rxin", "student")), (7L, ("jgonzal", "postdoc")),
-                           (5L, ("franklin", "prof")), (2L, ("istoica", "prof"))))
+    val users: RDD[(VertexId, (String, String, Int))] =
+      sc.parallelize(Array((3L, ("rxin", "student", 3)), (7L, ("jgonzal", "postdoc", 7)),
+                           (5L, ("franklin", "prof", 5)), (2L, ("istoica", "prof", 2))))
     // Create an RDD for edges
     val relationships: RDD[Edge[String]] =
       sc.parallelize(Array(Edge(3L, 7L, "collab"),    Edge(5L, 3L, "advisor"),
                            Edge(2L, 5L, "colleague"), Edge(5L, 7L, "pi")))
     // Define a default user in case there are relationship with missing user
-    val defaultUser = ("John Doe", "Missing")
+    val defaultUser = ("John Doe", "Missing", 0)
     // Build the initial Graph
     val graph = Graph(users, relationships, defaultUser)
     
     // Count all users which are postdocs
-    graph.vertices.filter { case (id, (name, pos)) => pos == "postdoc" }.count
+    graph.vertices.filter { case (id, (name, pos, count)) => pos == "postdoc" }.count
     // Count all the edges where src > dst
     graph.edges.filter(e => e.srcId > e.dstId).count
     
@@ -35,6 +35,24 @@ object GraphX {
       graph.triplets.map(triplet =>
         triplet.srcAttr._1 + " is the " + triplet.attr + " of " + triplet.dstAttr._1)
     facts.collect.foreach(println(_))
+    /**
+     * rxin is the collab of jgonzal
+     * franklin is the advisor of rxin
+     * istoica is the colleague of franklin
+     * franklin is the pi of jgonzal
+     */
+    
+    // aggregateMessage test
+    val aggregatedGraph: VertexRDD[Int] = graph.mapReduceTriplets[Int](
+      triplet => { Iterator((triplet.dstId, triplet.srcAttr._3)) },
+      (a, b) => (a + b)
+    )
+    aggregatedGraph.collect.foreach(println(_))
+    /**
+     * (5,2)
+     * (3,5)
+     * (7,8)
+     */
     
     sc.stop()
   }
