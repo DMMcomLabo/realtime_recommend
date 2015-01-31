0c56a0750b30844e954bf244fa286bb3e4a289b0
Nagato KASAKI <kasaki-nagato@dmm.com>
2015-01-31 15:23:50 +0900

    modify graph input format

--- src/main/scala/GraphX.scala
+++ src/main/scala/GraphX.scala
@@ -14,119 +14,97 @@ object GraphX {
     val sc = new SparkContext(conf)
     
     val initialMap:Map[Long, Double] = Map.empty
-    val nodes: RDD[(VertexId, (String, String, Double, Map[Long, Double]))] =
+    val initialMessage: (Map[Long, Double], Double) = (initialMap, 0.0)
+    val nodes: RDD[(VertexId, (String, String, Double, (Map[Long, Double], Double)))] =
       sc.parallelize(Array(
-          (10001L, ("word", "艦隊これくしょん", 0.8, initialMap)),
-          (10002L, ("word", "魔法少女まどか☆マギカ", 0.8, initialMap)),
-          (20001L, ("product", "艦隊これくしょん -艦これ- アンソロジーコミック 横須賀鎮守府編 （1）", 1.0, initialMap)),
-          (20002L, ("product", "艦隊これくしょん‐艦これ‐コミックアラカルト 舞鶴鎮守府編 壱", 1.0, initialMap)),
-          (20003L, ("product", "第1話 艦隊これくしょん-艦これ-", 1.0, initialMap)),
-          (20004L, ("product", "艦隊これくしょん-艦これ- 第1巻 限定版 【DMMオリジナル特典付き】（ブルーレイディスク）", 1.0, initialMap)),
-          (20005L, ("product", "劇場版 魔法少女まどか☆マギカ [前編] 始まりの物語", 1.0, initialMap)),
-          (20006L, ("product", "劇場版 魔法少女まどか☆マギカ [後編] 永遠の物語", 1.0, initialMap)),
-          (30001L, ("genre", "青年コミック", 1.0, initialMap)),
-          (30002L, ("genre", "2015年冬アニメ", 1.0, initialMap)),
-          (30003L, ("genre", "アクション", 1.0, initialMap)),
-          (30004L, ("genre", "2010年代", 1.0, initialMap)),
-          (30005L, ("genre", "テレビ", 1.0, initialMap)),
-          (30006L, ("genre", "魔法少女", 1.0, initialMap)),
-          (30007L, ("genre", "劇場版（アニメ映画）", 1.0, initialMap)),
-          (30008L, ("genre", "2010年代", 1.0, initialMap)),
-          (30009L, ("genre", "SF/ファンタジー", 1.0, initialMap))
+          (10001L, ("word", "艦隊これくしょん", 0.8, initialMessage)),
+          (10002L, ("word", "魔法少女まどか☆マギカ", 0.8, initialMessage)),
+          (20001L, ("product", "艦隊これくしょん -艦これ- アンソロジーコミック 横須賀鎮守府編 （1）", 1.0, initialMessage)),
+          (20002L, ("product", "艦隊これくしょん‐艦これ‐コミックアラカルト 舞鶴鎮守府編 壱", 1.0, initialMessage)),
+          (20003L, ("product", "第1話 艦隊これくしょん-艦これ-", 1.0, initialMessage)),
+          (20004L, ("product", "艦隊これくしょん-艦これ- 第1巻 限定版 【DMMオリジナル特典付き】（ブルーレイディスク）", 1.0, initialMessage)),
+          (20005L, ("product", "劇場版 魔法少女まどか☆マギカ [前編] 始まりの物語", 1.0, initialMessage)),
+          (20006L, ("product", "劇場版 魔法少女まどか☆マギカ [後編] 永遠の物語", 1.0, initialMessage)),
+          (30001L, ("genre", "青年コミック", 1.0, initialMessage)),
+          (30002L, ("genre", "2015年冬アニメ", 1.0, initialMessage)),
+          (30003L, ("genre", "アクション", 1.0, initialMessage)),
+          (30004L, ("genre", "2010年代", 1.0, initialMessage)),
+          (30005L, ("genre", "テレビ", 1.0, initialMessage)),
+          (30006L, ("genre", "魔法少女", 1.0, initialMessage)),
+          (30007L, ("genre", "劇場版（アニメ映画）", 1.0, initialMessage)),
+          (30008L, ("genre", "SF/ファンタジー", 1.0, initialMessage))
         ))
-    val edges: RDD[Edge[(String, Double)]] =
+    val edges: RDD[Edge[Double]] =
       sc.parallelize(Array(
-          (Edge(10001L, 20001L, ("search", 0.8))),
-          (Edge(10001L, 20002L, ("search", 0.7))),
-          (Edge(10001L, 20003L, ("search", 0.6))),
-          (Edge(10001L, 20004L, ("search", 0.5))),
-          (Edge(10002L, 20005L, ("search", 0.8))),
-          (Edge(10002L, 20006L, ("search", 0.7))),
-          (Edge(20001L, 30001L, ("attr", 0.7))),
-          (Edge(20002L, 30001L, ("attr", 0.7))),
-          (Edge(20003L, 30002L, ("attr", 0.7))),
-          (Edge(20004L, 30002L, ("attr", 0.7))),
-          (Edge(20003L, 30003L, ("attr", 0.7))),
-          (Edge(20004L, 30003L, ("attr", 0.7))),
-          (Edge(20003L, 30004L, ("attr", 0.7))),
-          (Edge(20004L, 30004L, ("attr", 0.7))),
-          (Edge(20005L, 30004L, ("attr", 0.7))),
-          (Edge(20006L, 30004L, ("attr", 0.7))),
-          (Edge(20003L, 30005L, ("attr", 0.7))),
-          (Edge(20004L, 30005L, ("attr", 0.7))),
-          (Edge(20005L, 30006L, ("attr", 0.7))),
-          (Edge(20006L, 30006L, ("attr", 0.7))),
-          (Edge(20005L, 30007L, ("attr", 0.7))),
-          (Edge(20006L, 30007L, ("attr", 0.7))),
-          (Edge(20003L, 30008L, ("attr", 0.7))),
-          (Edge(20004L, 30008L, ("attr", 0.7))),
-          (Edge(20005L, 30008L, ("attr", 0.7))),
-          (Edge(20006L, 30008L, ("attr", 0.7))),
-          (Edge(20003L, 30009L, ("attr", 0.7))),
-          (Edge(20004L, 30009L, ("attr", 0.7))),
-          (Edge(20005L, 30009L, ("attr", 0.7))),
-          (Edge(20006L, 30009L, ("attr", 0.7))),
-          (Edge(30001L, 10001L, ("cluster", 0.0))),
-          (Edge(30002L, 10001L, ("cluster", 0.0))),
-          (Edge(30003L, 10001L, ("cluster", 0.0))),
-          (Edge(30004L, 10001L, ("cluster", 0.0))),
-          (Edge(30005L, 10001L, ("cluster", 0.0))),
-          (Edge(30006L, 10001L, ("cluster", 0.0))),
-          (Edge(30007L, 10001L, ("cluster", 0.0))),
-          (Edge(30008L, 10001L, ("cluster", 0.0))),
-          (Edge(30009L, 10001L, ("cluster", 0.0))),
-          (Edge(30001L, 10002L, ("cluster", 0.0))),
-          (Edge(30002L, 10002L, ("cluster", 0.0))),
-          (Edge(30003L, 10002L, ("cluster", 0.0))),
-          (Edge(30004L, 10002L, ("cluster", 0.0))),
-          (Edge(30005L, 10002L, ("cluster", 0.0))),
-          (Edge(30006L, 10002L, ("cluster", 0.0))),
-          (Edge(30007L, 10002L, ("cluster", 0.0))),
-          (Edge(30008L, 10002L, ("cluster", 0.0))),
-          (Edge(30009L, 10002L, ("cluster", 0.0)))
+          (Edge(10001L, 20001L, 0.8)),
+          (Edge(10001L, 20002L, 0.7)),
+          (Edge(10001L, 20003L, 0.6)),
+          (Edge(10001L, 20004L, 0.5)),
+          (Edge(10002L, 20005L, 0.8)),
+          (Edge(10002L, 20006L, 0.7)),
+          (Edge(20001L, 30001L, 0.7)),
+          (Edge(20002L, 30001L, 0.7)),
+          (Edge(20003L, 30002L, 0.7)),
+          (Edge(20004L, 30002L, 0.7)),
+          (Edge(20003L, 30003L, 0.7)),
+          (Edge(20004L, 30003L, 0.7)),
+          (Edge(20005L, 30003L, 0.7)),
+          (Edge(20006L, 30003L, 0.7)),
+          (Edge(20003L, 30004L, 0.7)),
+          (Edge(20004L, 30004L, 0.7)),
+          (Edge(20005L, 30004L, 0.7)),
+          (Edge(20006L, 30004L, 0.7)),
+          (Edge(20003L, 30005L, 0.7)),
+          (Edge(20004L, 30005L, 0.7)),
+          (Edge(20005L, 30006L, 0.7)),
+          (Edge(20006L, 30006L, 0.7)),
+          (Edge(20005L, 30007L, 0.7)),
+          (Edge(20006L, 30007L, 0.7)),
+          (Edge(20004L, 30008L, 0.7)),
+          (Edge(20005L, 30008L, 0.7)),
+          (Edge(20006L, 30008L, 0.7))
         ))
     // Build the initial Graph
     val graph = Graph(nodes, edges)
     
     // Show triplets
-    graph.triplets.map(triplet =>
-        triplet.srcAttr._2 + " :- " + triplet.attr._2 + " -> " + triplet.dstAttr._2)
-        .collect.foreach(println(_))
+//    graph.triplets.map(triplet =>
+//        triplet.srcAttr._2 + " :- " + triplet.attr + " -> " + triplet.dstAttr._2)
+//        .collect.foreach(println(_))
     
     def mergeMap(map1: Map[Long, Double], map2: Map[Long, Double]):Map[Long, Double] =  {
       (map1.keySet ++ map2.keySet).map( i => (i, map1.getOrElse(i, 0.0) + map2.getOrElse(i, 0.0))).toMap
     }
     
-    def correlation(map1: Map[Long, Double], map2: Map[Long, Double]):Double =  {
-      val total = (map1.keySet ++ map2.keySet).map( i => (map1.getOrElse(i, 0.0) * map2.getOrElse(i, 0.0))).sum
-      val base = map2.values.sum
-      if (base > 0) {
-        total / base
-      } else {
-        0.0
-      }
-    }
-    
-    // Calculate correlation between word and genre
-    val newGraph = graph.pregel(initialMap, 1)(
+    val newGraph = graph.pregel(initialMessage, 2)(
       (id, VD, message) => VD.copy(_4 = message),
       triplet => { 
-        if (triplet.attr._1 == "search") {
-          Iterator((triplet.srcId, Map(triplet.dstId -> triplet.attr._2)))
-        } else if (triplet.attr._1 == "attr") {
-          Iterator((triplet.dstId, Map(triplet.srcId -> triplet.attr._2)))
+        if (triplet.srcAttr._1 == "word" && triplet.dstAttr._1 == "product") {
+          Iterator((triplet.dstId, (Map(triplet.srcId -> triplet.attr), 0.0)))
+        } else if (triplet.srcAttr._1 == "product" && triplet.dstAttr._1 == "genre") {
+          Iterator({
+              val oldMap = triplet.srcAttr._4._1
+              val attr = triplet.attr
+              val newMap:Map[Long, Double] = oldMap.keySet.map( i => (i, oldMap.getOrElse(i, 0.0) * attr)).toMap
+            (triplet.dstId, (newMap, triplet.attr))
+          })
         } else {
           Iterator.empty
         }
       },
-      (map1, map2) => mergeMap(map1, map2)
+      (msg1, msg2) => (mergeMap(msg1._1, msg2._1), msg1._2 + msg2._2)
     )
     // Show correlation
-    newGraph.triplets.filter(triplet => triplet.attr._1 == "cluster")
-    .map(triplet => {
-        triplet.srcAttr._2 + " :- " + correlation(triplet.srcAttr._4, triplet.dstAttr._4) + " -> " + triplet.dstAttr._2
-      }
-    ).collect.foreach(println(_))
+    newGraph.vertices.filter(v => v._2._1 == "genre")
+    .map( v => {
+        val id = v._1
+        val name = v._2._2
+        val wordmap = v._2._4._1
+        val weight = v._2._4._2
+        val modwordmap = wordmap.keySet.map( i => (i, wordmap.getOrElse(i, 0.0) / weight))
+        (id, name, modwordmap)
+      })
+    .collect.foreach(println(_))
     
     sc.stop()
   }
